a3978a08ac8a1a04ca7fe1bbe1b6dd6c39ea6bdd
Manenimabasi Udoh (Dev Mani) <85757137+manenim@users.noreply.github.com>
2024-11-06 11:14:03 +0100

    second commit

--- src/Payment.js
+++ src/Payment.js
@@ -9,14 +9,14 @@ function Payment() {
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:5252/config").then(async (r) => {
+    fetch("https://stripe-backend-flgq.onrender.com/config").then(async (r) => {
       const { publishableKey } = await r.json();
       setStripePromise(loadStripe(publishableKey));
     });
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:5252/create-payment-intent", {
+    fetch("https://stripe-backend-flgq.onrender.com/create-payment-intent", {
       method: "POST",
       body: JSON.stringify({}),
     }).then(async (result) => {
